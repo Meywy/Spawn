b37560f1b4aacb762231850d2d95f5f20fc79c2b
Meywy <meywyy@gmail.com>
2024-08-04 00:53:31 +0200

    Cleaned the code + changed state of Strings

--- src/main/java/dev/meywy/spawn/commands/SpawnCommand.java
+++ src/main/java/dev/meywy/spawn/commands/SpawnCommand.java
@@ -16,27 +16,35 @@ public class SpawnCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
+    private static final String Name = ChatColor.GRAY + "[" + ChatColor.GOLD + "Spawn" + ChatColor.GRAY + "] ";
+    private static final String NoPerms = Name + ChatColor.RED + "You do not have permissions!";
+
+    public static String getName() { return Name; }
+    public static String getNoPerms() { return NoPerms; }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
 
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            Location location = plugin.getConfig().getLocation("spawn");
-
-            if (location != null) {
-                if (player.hasPermission("spawn.spawn") || player.hasPermission("spawn.*")) {
-                    player.teleport(location);
-                    player.sendMessage(SetSpawnCommand.Name + ChatColor.GREEN + "You have been teleported to spawn.");
-                } else {
-                    player.sendMessage(SetSpawnCommand.Name + ChatColor.RED + "You do not have permissions!");
-                }
-            } else {
-                player.sendMessage(SetSpawnCommand.Name + ChatColor.RED + "The spawn location is not set!");
-            }
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
+            return true;
         }
 
+        Location l = plugin.getConfig().getLocation("spawn");
+        boolean hasPermission = p.hasPermission("spawn.spawn") || p.hasPermission("spawn.*");
 
-        return false;
+        if (!hasPermission) {
+            p.sendMessage(NoPerms);
+            return true;
+        }
+
+        if (l == null) {
+            p.sendMessage(Name + ChatColor.RED + "The spawn location is not set!");
+            return true;
+        }
+
+        p.teleport(l);
+        p.sendMessage(Name + ChatColor.GREEN + "You have been teleported to spawn.");
+        return true;
     }
 }
